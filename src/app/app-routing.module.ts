e144bfa3d36079f9637e3dbfd4f535bed5e3a3c7
Daedite <espoir.ditekemena@dae.mn>
2022-12-26 16:09:03 +0200

    first commit.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {VdoPlayerComponent} from "./components/vdo-player/vdo-player.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'compose',
+    component: VdoPlayerComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {VgCoreModule} from "@videogular/ngx-videogular/core";
+import {VgBufferingModule} from "@videogular/ngx-videogular/buffering";
+import {VgOverlayPlayModule} from "@videogular/ngx-videogular/overlay-play";
+import {VgControlsModule} from "@videogular/ngx-videogular/controls";
+import { VdoPlayerComponent } from './components/vdo-player/vdo-player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VdoPlayerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
